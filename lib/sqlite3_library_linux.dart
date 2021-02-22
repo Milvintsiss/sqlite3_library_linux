42e3a6d8750b024fc7703cc88fa78ab0f3ec73df
Milvintsiss <Milvintsiss@users.noreply.github.com>
2021-02-22 20:10:55 +0100

    fix default and old sqlite3 library file

--- lib/sqlite3_library_linux.dart
+++ lib/sqlite3_library_linux.dart
@@ -5,22 +5,19 @@ import 'dart:io' show File, Platform;
 
 ///relative path to default SQLite3 library file
 const default_sqlite3_linux_libraryPath =
-    '/data/flutter_assets/packages/sqlite3_library_linux/default/libsqlite3.so';
+    '/data/flutter_assets/packages/sqlite3_library_linux/default_libsqlite3.so';
 
 ///relative path to old SQLite3 library file
 const old_sqlite3_linux_libraryPath =
-    '/data/flutter_assets/packages/sqlite3_library_linux/debian/libsqlite3.so';
+    '/data/flutter_assets/packages/sqlite3_library_linux/old_libsqlite3.so';
 
 ///This function open SQLite3 in memory and return the associated DynamicLibrary
 ///object.
-///
-/// You can obtain [sqliteLibraryPath] by running [getSQLiteLibraryPathOnLinux]
 DynamicLibrary openSQLiteOnLinux() {
   DynamicLibrary library;
 
   String executableDirectoryPath =
       File(Platform.resolvedExecutable).parent.path;
-
   print('executableDirectoryPath: $executableDirectoryPath');
   try {
     String sqliteLibraryPath =
@@ -35,7 +32,7 @@ DynamicLibrary openSQLiteOnLinux() {
       print(e);
       //some linux distributions like debian cannot run the first version
       print(_red("Failed to load SQLite3, "
-          "trying to load an old version of the SQLite3..."));
+          "trying to load an old version of SQLite3..."));
       String sqliteLibraryPath =
           executableDirectoryPath + old_sqlite3_linux_libraryPath;
       print('SQLite3LibraryPath: $sqliteLibraryPath');
